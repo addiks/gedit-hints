8212d38d6dc3c8d6c9ba2a98265e9cfbce9fba39
Gerrit Addiks <gerrit.addiks@brille24.de>
2015-10-01 14:43:14 +0200

    initial commit

---
+++ addiks_hints/gladehandler.py
@@ -0,0 +1,208 @@
+# Copyright (C) 2015 Gerrit Addiks <gerrit@addiks.net>
+# https://github.com/addiks/gedit-window-management
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import GLib, Gtk, Gdk
+from os.path import expanduser
+import traceback
+
+class GladeHandler:
+
+    def __init__(self, plugin, builder):
+        self._plugin  = plugin
+        self._builder = builder
+
+    ### CONFIGURATION
+
+    def onConfigPHPLintActivate(self, switch, userData=None):
+        builder = self._builder
+        switch = builder.get_object("switchPHPLint")
+        active = switch.get_active()
+        self._plugin.set_config("phplint", active)
+
+    def onConfigPHPMDActivate(self, switch, userData=None):
+        builder = self._builder
+        switch = builder.get_object("switchPHPMD")
+        active = switch.get_active()
+        self._plugin.set_config("phpmd", active)
+
+    def onPHPMDConfigureClicked(self, button):
+        self._plugin.show_phpmd_config()
+
+    def onPHPCSConfigureClicked(self, button):
+        self._plugin.show_phpcs_config()
+
+    ### PHPMD CONFIGURATION
+
+    def onPHPMDAdd(self, button):
+        builder = self._builder
+        window  = builder.get_object("windowPHPMDConfig")
+
+        dialog = Gtk.Dialog("Add ruleset for directory", window)
+        dialog.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        dialog.add_button(Gtk.STOCK_OK,     Gtk.ResponseType.OK)
+        dialogBox = dialog.get_content_area()
+
+        dialogGrid = Gtk.Grid()
+        dialogBox.pack_end(dialogGrid, True, True, 0);
+
+        labelDirectory = Gtk.Label("Directory that the ruleset applies for:")
+        dialogGrid.attach(labelDirectory, 0, 0, 1, 1)
+
+        userEntryDirectory = Gtk.Entry()
+        dialogGrid.attach(userEntryDirectory, 1, 0, 1, 1)
+
+        buttonBrowseDirectory = Gtk.Button("browse")
+        buttonBrowseDirectory.userEntry = userEntryDirectory
+        buttonBrowseDirectory.connect("button_press_event", self.__select_folder)
+        dialogGrid.attach(buttonBrowseDirectory, 2, 0, 1, 1)
+
+        labelRuleset = Gtk.Label("Path to PHP-MD ruleset:")
+        dialogGrid.attach(labelRuleset, 0, 1, 1, 1)
+
+        userEntryRuleset = Gtk.Entry()
+        dialogGrid.attach(userEntryRuleset, 1, 1, 1, 1)
+
+        buttonBrowseRuleset = Gtk.Button("browse")
+        buttonBrowseRuleset.userEntry = userEntryRuleset
+        buttonBrowseRuleset.connect("button_press_event", self.__select_file)
+        dialogGrid.attach(buttonBrowseRuleset, 2, 1, 1, 1)
+
+        dialog.show_all()
+        response  = dialog.run()
+        directory = userEntryDirectory.get_text()
+        ruleset   = userEntryRuleset.get_text()
+        dialog.destroy()
+
+        if (response == Gtk.ResponseType.OK) and directory != '' and ruleset != '':
+            self._plugin.add_phpmd_ruleset(ruleset, directory)
+            self._plugin.update_phpmd_ruleset_treeview()
+
+    def onPHPMDRemove(self, button):
+        builder = self._builder
+
+        treeview  = builder.get_object("treeviewPHPMDConfig")
+        liststore = builder.get_object("liststorePHPMDConfig")
+
+        selection = treeview.get_selection()
+
+        store, selected_rows = selection.get_selected_rows()
+
+        for path in selected_rows:
+            treeIter = liststore.get_iter(path)
+            directory = liststore.get_value(treeIter, 0)
+            ruleset   = liststore.get_value(treeIter, 1)
+            self._plugin.remove_phpmd_ruleset(ruleset, directory)
+        self._plugin.update_phpmd_ruleset_treeview()
+
+    ### PHPCS CONFIGURATION
+
+    def onPHPCSAdd(self, button):
+        builder = self._builder
+        window  = builder.get_object("windowPHPCSConfig")
+
+        dialog = Gtk.Dialog("Add ruleset for directory", window)
+        dialog.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        dialog.add_button(Gtk.STOCK_OK,     Gtk.ResponseType.OK)
+        dialogBox = dialog.get_content_area()
+
+        dialogGrid = Gtk.Grid()
+        dialogBox.pack_end(dialogGrid, True, True, 0);
+
+        labelDirectory = Gtk.Label("Directory that the ruleset applies for:")
+        dialogGrid.attach(labelDirectory, 0, 0, 1, 1)
+
+        userEntryDirectory = Gtk.Entry()
+        dialogGrid.attach(userEntryDirectory, 1, 0, 1, 1)
+
+        buttonBrowseDirectory = Gtk.Button("browse")
+        buttonBrowseDirectory.userEntry = userEntryDirectory
+        buttonBrowseDirectory.connect("button_press_event", self.__select_folder)
+        dialogGrid.attach(buttonBrowseDirectory, 2, 0, 1, 1)
+
+        labelRuleset = Gtk.Label("Path to PHP-CS ruleset or predefined standard:")
+        dialogGrid.attach(labelRuleset, 0, 1, 1, 1)
+
+        userEntryRuleset = Gtk.Entry()
+        dialogGrid.attach(userEntryRuleset, 1, 1, 1, 1)
+
+        buttonBrowseRuleset = Gtk.Button("browse")
+        buttonBrowseRuleset.userEntry = userEntryRuleset
+        buttonBrowseRuleset.connect("button_press_event", self.__select_file)
+        dialogGrid.attach(buttonBrowseRuleset, 2, 1, 1, 1)
+
+        description = "Known standards: PHPCS, PSR1, Zend, PSR2, MySource, Squiz and PEAR"
+        descriptionLabel = Gtk.Label(description)
+        dialogGrid.attach(descriptionLabel, 0, 2, 3, 1)
+
+        dialog.show_all()
+        response  = dialog.run()
+        directory = userEntryDirectory.get_text()
+        ruleset   = userEntryRuleset.get_text()
+        dialog.destroy()
+
+        if (response == Gtk.ResponseType.OK) and directory != '' and ruleset != '':
+            self._plugin.add_phpcs_ruleset(ruleset, directory)
+            self._plugin.update_phpcs_ruleset_treeview()
+
+    def onPHPCSRemove(self, button):
+        builder = self._builder
+
+        treeview  = builder.get_object("treeviewPHPCSConfig")
+        liststore = builder.get_object("liststorePHPCSConfig")
+
+        selection = treeview.get_selection()
+
+        store, selected_rows = selection.get_selected_rows()
+
+        for path in selected_rows:
+            treeIter = liststore.get_iter(path)
+            directory = liststore.get_value(treeIter, 0)
+            ruleset   = liststore.get_value(treeIter, 1)
+            self._plugin.remove_phpcs_ruleset(ruleset, directory)
+        self._plugin.update_phpcs_ruleset_treeview()
+
+    ### HELPER
+
+    def __select_folder(self, entry, event=None):
+        return self.__select_file(entry, event, True)
+
+    def __select_file(self, entry, event=None, isFolder=False):
+        if event.button == 1:
+            builder = self._builder
+
+            window = entry.get_toplevel()
+
+            if hasattr(entry, "userEntry"):
+                entry = entry.userEntry
+
+            action = Gtk.FileChooserAction.OPEN
+            if isFolder:
+                action = Gtk.FileChooserAction.SELECT_FOLDER
+
+            title = "Choose file"
+            if isFolder:
+                title = "Choose folder"
+
+            dialog = Gtk.FileChooserDialog(title, window, action)
+            dialog.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+            dialog.add_button(Gtk.STOCK_OK,     Gtk.ResponseType.OK)
+
+            response = dialog.run()
+            if response == Gtk.ResponseType.OK:
+                entry.set_text(dialog.get_filename())
+            dialog.destroy()
+
+
---
+++ hintmanager.py
@@ -0,0 +1,174 @@
+
+import re
+import os
+import subprocess
+import tempfile
+from addiks_hints.helpers import *
+from subprocess import Popen, PIPE
+import xml.etree.ElementTree as ET
+
+class HintManager:
+
+    def __init__(self, plugin, data_dir):
+        self._adapters = {
+            'phplint': PHPLintAdapter(plugin, data_dir),
+            'phpmd':   PHPMDAdapter(plugin, data_dir),
+            'phpcs':   PHPCSAdapter(plugin, data_dir),
+        }
+        self._active_adapters = []
+
+    def set_adapter_state(self, name, is_active):
+        if is_active and name not in self._active_adapters:
+            self._active_adapters.append(name)
+        elif not is_active and name in self._active_adapters:
+            self._active_adapters.remove(name)
+
+    def get_all_adapter_names(self):
+        names = []
+        for adapterName in self._adapters:
+            names.append(adapterName)
+        return names
+
+    def get_hints_by_file(self, filepath, content=None):
+        hints = []
+
+        suffix = filepath.split(".")[-1]
+
+        tempFile = tempfile.NamedTemporaryFile(suffix="."+suffix, delete=False)
+
+        if content == None:
+            tempFile.file.write(file_get_contents(filepath))
+        else:
+            tempFile.file.write(bytes(content, 'UTF-8'))
+
+        tempFilePath = tempFile.name
+        tempFile.close()
+
+        for adapterName in self._adapters:
+            if adapterName in self._active_adapters:
+                adapter = self._adapters[adapterName]
+                newHints = adapter.get_hints_by_file(tempFile.name, filepath)
+                hints = hints + newHints
+
+        os.remove(tempFilePath)
+
+        hints = sorted(hints, key=lambda row: row[6])
+
+        return hints
+
+
+class PHPLintAdapter:
+
+    def __init__(self, plugin, data_dir):
+        pass
+
+    def get_hints_by_file(self, filepath, filepathReal):
+        hints = []
+        
+        if filepathReal[-4:]!='.php':
+            return hints
+
+        plugin_path = os.path.dirname(__file__)
+
+        color = "#FF0000"
+
+        try:
+            sp = subprocess.Popen(["/usr/bin/env", "php", plugin_path+"/PHP-Parser/bin/php-parse.php", "-c", "--no-dump", filepath], 
+                stdin=PIPE, stdout=PIPE, stderr=PIPE
+            )
+            sp.wait()
+            output, err = sp.communicate()
+            output = output.decode()
+            lines = output.split("\n")
+            pattern = re.compile("(\=+\> )?(.*) from (\d+)\:(\d+) to (\d+)\:(\d+)")
+            for line in lines:
+                match = pattern.match(line)
+                if match != None:
+                    message, lineBegin, columnBegin, lineEnd, columnEnd = match.group(2, 3, 4, 5, 6)
+                    lineBegin = int(lineBegin)-1
+                    lineEnd = int(lineEnd)-1
+                    columnBegin = int(columnBegin)-1
+                    columnEnd = int(columnEnd)
+                    hints.append([lineBegin, lineEnd, columnBegin, columnEnd, message, color, 300])
+        except OSError as error:
+            print(error)
+        
+        return hints
+
+class PHPMDAdapter:
+
+    def __init__(self, plugin, data_dir):
+        self._data_dir = data_dir
+        self._plugin = plugin
+
+    def get_hints_by_file(self, filepath, filepathReal):
+        hints = []
+
+        if filepathReal[-4:]!='.php':
+            return hints
+
+        color = "#8F7811"
+
+        for directory, rulesetFilepath in self._plugin.get_all_phpmd_rulesets():
+            if filepathReal[0:len(directory)] == directory and os.path.exists(rulesetFilepath):
+                try:
+                    sp = subprocess.Popen(['phpmd', filepath, 'xml', rulesetFilepath], 
+                        stdin=PIPE, stdout=PIPE, stderr=PIPE
+                    )
+                    sp.wait()
+                    output, err = sp.communicate()
+                    output = output.decode()
+                    rootXml = ET.fromstring(output)
+                    for fileXml in rootXml:
+                        for violationXml in fileXml:
+                            lineBegin   = int(violationXml.attrib['beginline'])-1
+                            lineEnd     = int(violationXml.attrib['beginline'])-1 #violationXml.attrib['endline']
+                            columnBegin = 0
+                            columnEnd   = 999
+                            message     = violationXml.text.strip()
+                            hints.append([lineBegin, lineEnd, columnBegin, columnEnd, message, color, 200])
+                except OSError as error:
+                    print(error)
+        return hints
+
+
+class PHPCSAdapter:
+
+    def __init__(self, plugin, data_dir):
+        self._data_dir = data_dir
+        self._plugin = plugin
+
+    def get_hints_by_file(self, filepath, filepathReal):
+        hints = []
+
+        color = "#A5A5A5"
+
+        if filepathReal[-4:]!='.php':
+            return hints
+
+        for directory, rulesetFilepath in self._plugin.get_all_phpcs_rulesets():
+            if filepathReal[0:len(directory)] == directory:
+                try:
+                    sp = subprocess.Popen(['phpcs', '--report=xml', '--standard='+rulesetFilepath, filepath],  # TODO
+                        stdin=PIPE, stdout=PIPE, stderr=PIPE
+                    )
+                    sp.wait()
+                    output, err = sp.communicate()
+                    output = output.decode()
+                    if len(output)>0:
+                        rootXml = ET.fromstring(output)
+                        for fileXml in rootXml:
+                            for errorXml in fileXml:
+                                lineBegin   = int(errorXml.attrib['line'])-1
+                                lineEnd     = int(errorXml.attrib['line'])-1
+                                columnBegin = int(errorXml.attrib['column'])-1
+                                columnEnd   = int(errorXml.attrib['column'])
+                                message     = errorXml.text.strip()
+
+                                if columnBegin < 1:
+                                    columnBegin = 1
+
+                                hints.append([lineBegin, lineEnd, columnBegin, columnEnd, message, color, 100])
+                except OSError as error:
+                    print(error)
+        return hints
